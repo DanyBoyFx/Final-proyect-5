379563adb0798dc015949098b775df027b6824d4
DanyBoyFx <daniel.zermeno007@gmail.com>
2024-04-21 15:11:21 -0600

    Final

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Final-proyect-5
+# Final-proyect-Sprint-5
+En esta página vamos a poder ver dos tipos de gráficos, un histograma y otro de dispersión los cuales comparan la relación precio-kilometraje
+https://final-proyect-sprint-5.onrender.com
---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pandas as pd
+import plotly_express as px
+
+car_data = pd.read_csv('E:\\Visual\\Jupyter\\Sprint 5\\Proyecto final\\Final-proyect-Sprint-5\\vehicles_us.csv')
+hist_button = st.button('Construir histograma') # crear un botón
+graph_button = st.button('Construir gráfico de dispersión') # crear un botón
+
+#Button 1
+if hist_button: # al hacer clic en el botón
+    # escribir un mensaje
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+        
+    # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#Button 2
+if graph_button: #al hacer click en el botón
+    # escribir un mensaje
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    #crear el gráfico
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    #mostrar gráfico
+    st.plotly_chart(fig, use_container_width=True)
+
+#Checkbox 1
+build_histogram = st.checkbox('Construir un histograma')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    
+    # escribir un mensaje
+    st.write('Construir un histograma para la columna odómetro')
+     # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+    
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
+
+#Checkbox 2
+build_histogram = st.checkbox('Construir un gráfico')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    
+    # escribir un mensaje
+    st.write('Construir un gráfico para la columna odómetro')
+    
+    #crear el gráfico
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    #mostrar gráfico
+    st.plotly_chart(fig, use_container_width=True)
+    
